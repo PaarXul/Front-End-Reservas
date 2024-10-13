f7e32e4e241797e8cfebab1f63e199a5800c28df
Pablo Elgueta <pablo.elgueta@externos.hdi.cl>
2024-10-12 22:27:44 -0300

    PEV: Se agrega navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {FormsModule} from '@angular/forms';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatLineModule} from "@angular/material/core";
-import {NgxUiLoaderModule , NgxUiLoaderHttpModule } from "ngx-ui-loader";
-import {MatRadioModule} from '@angular/material/radio';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatLineModule } from '@angular/material/core';
+import { NgxUiLoaderModule, NgxUiLoaderHttpModule } from 'ngx-ui-loader';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatTableExporterModule } from 'mat-table-exporter';
-import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
-import {MatTabsModule} from "@angular/material/tabs";
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTabsModule } from '@angular/material/tabs';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { EventListComponent } from './components/event-list/event-list.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
@@ -36,45 +36,44 @@ import { CreateEventComponent } from './components/create-event/create-event.com
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     EventListComponent,
     CreateEventComponent,
-
+    NavbarComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        MatFormFieldModule,
-        MatInputModule,
-        FormsModule,
-        HttpClientModule,
-        MatSnackBarModule,
-        MatCardModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatListModule,
-        MatSlideToggleModule,
-        MatSelectModule,
-        MatProgressSpinnerModule,
-        MatDividerModule,
-        MatRadioModule,
-        MatCheckboxModule,
-        ReactiveFormsModule,
-        MatTableModule,
-        MatSortModule,
-        MatTableExporterModule,
-        MatLineModule,
-        NgxUiLoaderModule,
-        MatPaginatorModule,
-        MatSortModule,
-        NgxUiLoaderHttpModule.forRoot({
-            showForeground: true
-        }),
-        MatTabsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatDividerModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatSortModule,
+    MatTableExporterModule,
+    MatLineModule,
+    NgxUiLoaderModule,
+    MatPaginatorModule,
+    MatSortModule,
+    NgxUiLoaderHttpModule.forRoot({
+      showForeground: true,
+    }),
+    MatTabsModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
